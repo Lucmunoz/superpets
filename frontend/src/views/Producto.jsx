c9bd1f07221e5fe9aada3602a464857a20ab9cb1
Paula Vergara <paulavergara@hotmail.es>
2024-08-25 18:14:17 -0400

    Se añade contenido de vista Producto.jsx

--- frontend/src/views/Producto.jsx
+++ frontend/src/views/Producto.jsx
@@ -1,6 +1,20 @@
 const Producto = () => {
   return (
-    <div>Producto</div>
+    <main className='mainProducto'>
+      <div className='divDetalleProducto'>
+        <div>
+          <img src='/disfraz-salchicha.jpg' alt='producto-detalle' />
+        </div>
+        <div style={{ textAlign: 'center' }}>
+          <div className='divTextProducto'>
+            <h1 className='m-0'>Nombre Producto</h1>
+            <span>SKU-000</span>
+            <p className='pt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, autem praesentium! Cumque, dolorem? Consequuntur repellat, repellendus obcaecati recusandae facere asperiores?</p>
+          </div>
+          <button type='button' className='btn btn-danger'>Agregar al carro</button>
+        </div>
+      </div>
+    </main>
   )
 }
 
