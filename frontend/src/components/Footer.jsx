8067441793f543fc77cc99ddf3cdd6e864324996
Paula Vergara <paulavergara@hotmail.es>
2024-08-24 23:12:51 -0400

    Se añade componente Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <p>Redes Sociales</p>
+      </div>
+      <div>
+        <p>
+          Enlaces útiles
+        </p>
+      </div>
+      <div>
+        <p>Información de Contacto</p>
+      </div>
+
+    </footer>
+  )
+}
+
+export default Footer
