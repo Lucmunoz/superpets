e4a947820b75d238e2efad9d041ec82602f59607
Paula Vergara <paulavergara@hotmail.es>
2024-08-25 23:58:44 -0400

    Se añada contenido a vista InformacionPersonal.jsx, se agregan estilos y media queries first-mobile

--- frontend/src/views/InformacionPersonal.jsx
+++ frontend/src/views/InformacionPersonal.jsx
@@ -1,7 +1,29 @@
 const InformacionPersonal = () => {
-  const control = () => {}
+  const eliminarcuenta = () => {}
   return (
-    <main className='mainPersonal' />
+    <main>
+      <div className='divInfoPersonal'>
+        <h1>Información Personal</h1>
+        <p>Nombre </p>
+        <span>Juan López</span>
+        <hr />
+        <p>Email</p>
+        <span>juanitolopez@correo.com</span>
+        <hr />
+        <p>RUT</p>
+        <span>20.000.000-k</span>
+        <hr />
+        <p>Teléfono</p>
+        <span>+5912345679</span>
+        <hr />
+        <p>Dirección</p>
+        <span>Los Claveles 17, Concepción</span>
+        <hr />
+        <br />
+        <br />
+        <button type='button' className='btn btn-danger' onClick={eliminarcuenta()}>Eliminar Cuenta</button>
+      </div>
+    </main>
   )
 }
 
