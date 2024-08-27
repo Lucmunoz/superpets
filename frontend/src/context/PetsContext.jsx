5187b733a808b2d816df57affd8f893455886c03
Paula Vergara <paulavergara@hotmail.es>
2024-08-27 02:37:29 -0400

    Se crea y provee contexto

---
+++ frontend/src/context/PetsContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useState, useEffect } from 'react'
+
+export const PetsContext = createContext()
+
+const PetsContextProvider = ({ children }) => {
+  const [productos, setProductos] = useState([])
+  const [select, setSelect] = useState('az')
+
+  const getData = async () => {
+    try {
+      const response = await fetch('/productos.json')
+      const data = await response.json()
+      setProductos(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const cambiarFavorito = (id) => {
+    const nuevosProductos = [...productos]
+    const index = nuevosProductos.findIndex((p) => p.id === id)
+    nuevosProductos[index].isFavorite = !nuevosProductos[index].isFavorite
+    setProductos(nuevosProductos)
+  }
+
+  const cambiarSelect = (e) => setSelect(e.target.value)
+
+  const globalState = {
+    productos,
+    cambiarFavorito,
+    select,
+    cambiarSelect
+  }
+
+  return (
+    <PetsContext.Provider value={globalState}>
+      {children}
+    </PetsContext.Provider>
+  )
+}
+
+export default PetsContextProvider
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { HashRouter } from 'react-router-dom'
+import PetsContextProvider from './context/PetsContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <PetsContextProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </PetsContextProvider>
   </React.StrictMode>
 )
