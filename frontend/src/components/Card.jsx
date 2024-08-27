07b271e3f8640d6ea5f46b7599c8d29eb3e9eb23
Paula Vergara <paulavergara@hotmail.es>
2024-08-27 00:09:15 -0400

    Se ajusta Home y Card con información de JSON

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,15 +1,37 @@
+import { useEffect, useState } from 'react'
+
 const Card = () => {
+  const [productos, setProductos] = useState([])
+  const getData = async () => {
+    try {
+      const response = await fetch('/productos.json')
+      const data = await response.json()
+      setProductos(data)
+      console.log(productos)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
-    <div className='card'>
-      <img src='/disfraz-salchicha.jpg' className='card-img-top' alt='disfraz-salchicha' />
-      <div className='card-body'>
-        <h5 className='card-title'>Disfraz perro salchicha</h5>
-        <p className='card-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, eius?</p>
-      </div>
-      <div>
-        <button className='buttonCard'>Ver más</button>
-      </div>
-    </div>
+    <>
+      {productos.slice(6).map((p) =>
+        <div className='card' key={p.id}>
+          <img src={p.img} className='card-img-top' alt='disfraz-salchicha' />
+          <div className='card-body'>
+            <h5 className='card-title'>{p.nombre}</h5>
+            <p className='card-text'>{p.descripcion}?</p>
+          </div>
+          <div>
+            <button className='buttonCard'>Ver más</button>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -6,9 +6,8 @@ const Home = () => {
     <main className='mainHome'>
       <div>
         <Carrusel />
+        <h1 className='text-white text-center pt-5'>Destacados SuperPets<i className='fa-solid fa-paw fa-md ps-2' /> </h1>
         <div className='d-flex gap-5 justify-content-center divCardHome'>
-          <Card />
-          <Card />
           <Card />
         </div>
       </div>
