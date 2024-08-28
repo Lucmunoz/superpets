26dcbd78fe8b63a8d77a65fb863b06792c8c4505
Paula Vergara <paulavergara@hotmail.es>
2024-08-28 00:34:21 -0400

    Correción en logo

--- frontend/src/views/AcercaDeNosotros.jsx
+++ frontend/src/views/AcercaDeNosotros.jsx
@@ -1,6 +1,17 @@
 const AcercaDeNosotros = () => {
   return (
-    <div>AcercaDeNosotros</div>
+    <main>
+      <div className='divAcerca'>
+        <div className='divTextAcerca'>
+          <h1>Acerca de nosotros</h1>
+          <p> Somos una Tienda ubicada en el centro de Concepción, ofrecemos una amplia gama de productos para mascotas, desde juguetes y alimentos hasta artículos de aseo. Nos especializamos en brindar calidad y variedad para asegurar el bienestar y la felicidad de tus compañeros. ¡Encuentra todo lo que necesitas en un solo lugar!</p>
+        </div>
+        <div className='divYoutuve'>
+          <iframe width='300' height='170' src='https://www.youtube.com/embed/H4JE6XDR6UE?si=iPKlF7AivOtnirnN' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen />
+        </div>
+      </div>
+    </main>
+
   )
 }
 
