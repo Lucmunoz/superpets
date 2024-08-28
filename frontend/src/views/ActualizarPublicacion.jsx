3b99c7ea3120de7a24607eb0e0d155c701c134d0
Lucas <lucas.munoz.vasquez@gmail.com>
2024-08-27 21:32:35 -0400

    carga vistas crear publicacion y actualizar publicacion

--- frontend/src/views/ActualizarPublicacion.jsx
+++ frontend/src/views/ActualizarPublicacion.jsx
@@ -1,6 +1,37 @@
 const ActualizarPublicacion = () => {
   return (
-    <div>ActualizarPublicacion</div>
+    <main className='d-flex align-items-center'>
+      <div className='container-fluid col-11 col-sm-auto my-3 bg-white'>
+        <div className='p-4 py-4'>
+          <div className='d-flex justify-content-center pb-3'>
+            <h2 className=''>Actualizar publicación</h2>
+          </div>
+          <form>
+            <div className='form-group pb-3'>
+              <label htmlFor='exampleFormControlInput1'>Nombre producto </label>
+              <input type='text' className='d-sm-none form-control fst-italic' id='nombre' placeholder='...' />
+              <input type='text' className='d-none d-sm-flex form-control fst-italic' style={{ width: '467px' }} id='nombre' placeholder='...' />
+            </div>
+            <div className='form-group pb-3'>
+              <label htmlFor='exampleFormControlTextarea1'>Descripción del producto</label>
+              <textarea className='form-control fst-italic' id='descripcion' rows='3' placeholder='...' />
+            </div>
+            <div className='form-group pb-3'>
+              <label htmlFor='exampleFormControlTextarea1'>Descripción del producto</label>
+              <input type='number' className='form-control fst-italic' id='valor' placeholder='...' />
+            </div>
+            <div className='form-group pb-3'>
+              <label htmlFor='exampleFormControlInput1'>Ingresa la URL de tu imagen</label>
+              <input type='text' className='form-control fst-italic' id='img-url' placeholder='...' />
+            </div>
+            <div className='d-flex gap-2 justify-content-center'>
+              <button type='submit' className='btn btn-sm btn-success'>Guardar Cambios</button>
+              <button type='submit' className='btn btn-sm btn-danger'>Eliminar Publicación</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </main>
   )
 }
 
--- frontend/src/views/CrearPublicacion.jsx
+++ frontend/src/views/CrearPublicacion.jsx
@@ -1,6 +1,36 @@
 const CrearPublicacion = () => {
   return (
-    <div>CrearPublicacion</div>
+    <main className='d-flex align-items-center'>
+      <div className='container-fluid col-11 my-3 col-sm-auto bg-white'>
+        <div className='p-4 py-4'>
+          <div className='d-flex justify-content-center pb-3'>
+            <h2 className='text-center'>Crear Publicación <i class='fa-regular fa-square-plus' /></h2>
+          </div>
+          <form>
+            <div className='form-group pb-3'>
+              <label htmlFor='exampleFormControlInput1'>Nombre del producto </label>
+              <input type='text' className='d-sm-none form-control fst-italic' id='nombre' placeholder='Ingresa el Nombre de tu producto...' />
+              <input type='text' className='d-none d-sm-flex form-control fst-italic' style={{ width: '467px' }} id='nombre' placeholder='Ingresa el Nombre de tu producto...' />
+            </div>
+            <div className='form-group pb-3'>
+              <label htmlFor='exampleFormControlTextarea1'>Descripción del producto</label>
+              <textarea className='form-control fst-italic' id='descripcion' rows='3' placeholder='Ingresa el mayor detalle posible de tu producto para que los usuarios no tengan ninguna duda!' />
+            </div>
+            <div className='form-group pb-3'>
+              <label htmlFor='exampleFormControlTextarea1'>Descripción del producto</label>
+              <input type='number' className='form-control fst-italic' id='valor' placeholder='Ingresa el valor de venta...' />
+            </div>
+            <div className='form-group pb-3'>
+              <label htmlFor='exampleFormControlInput1'>Ingresa la URL de tu imagen</label>
+              <input type='text' className='form-control fst-italic' id='img-url' placeholder='Ingresa la URL de tu imagen...' />
+            </div>
+            <div className='text-center'>
+              <button type='submit' className='btn btn-danger'>Crear Publicación</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </main>
   )
 }
 
