1fad745ac89e01cd55f99707e318b90edaa981c0
Paula Vergara <paulavergara@hotmail.es>
2024-08-28 01:33:35 -0400

    Se corrige scroll horizontal en vista home

--- frontend/src/components/Carrusel.jsx
+++ frontend/src/components/Carrusel.jsx
@@ -8,13 +8,13 @@ const Carrusel = () => {
       </div>
       <div className='carousel-inner'>
         <div className='carousel-item active'>
-          <img src='/banner-superpets-01.png' className='d-block imgCarrusel' alt='proplan' />
+          <img src='/banner-superpets-01.jpg' className='d-block imgCarrusel' alt='proplan' />
         </div>
         <div className='carousel-item'>
-          <img src='/banner-superpets-02.png' className='d-block imgCarrusel' alt='proplan2' />
+          <img src='/banner-superpets-02.jpg' className='d-block imgCarrusel' alt='proplan2' />
         </div>
         <div className='carousel-item'>
-          <img src='/banner-superpets-03.png' className='d-block imgCarrusel' alt='proplan3' />
+          <img src='/banner-superpets-03.jpg' className='d-block imgCarrusel' alt='proplan3' />
         </div>
       </div>
       <button className='carousel-control-prev' type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide='prev'>
