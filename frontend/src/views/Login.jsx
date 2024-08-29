c4056a5baa978b1a66bab35b7910ece11b2799c2
Lucas <lucas.munoz.vasquez@gmail.com>
2024-08-28 23:30:36 -0400

    se añade logica a vista login

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -1,4 +1,8 @@
-import { useState } from 'react'
+// import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useState, useContext } from 'react'
+import { PetsContext } from '../context/PetsContext'
+// import { ENDPOINT } from '../config/constans'
 
 const credencialesUsuario = {
   correo: '',
@@ -7,19 +11,36 @@ const credencialesUsuario = {
 const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
 
 const Login = () => {
-  const [user, setUser] = useState(credencialesUsuario)
-  const handleUser = (event) => setUser({ ...user, [event.target.name]: event.target.value })
+  const navigate = useNavigate()
+  const [userTemp, setUserTemp] = useState(credencialesUsuario)
+  const { setUsuario } = useContext(PetsContext)
+  const handleUser = (event) => setUserTemp({ ...userTemp, [event.target.name]: event.target.value })
 
   const handleForm = (event) => {
     event.preventDefault()
 
-    if (!user.correo.trim() || !user.contrasena.trim()) {
+    if (!userTemp.correo.trim() || !userTemp.contrasena.trim()) {
       return window.alert('Email y password obligatorias.')
     }
 
-    if (!emailRegex.test(user.correo)) {
+    if (!emailRegex.test(userTemp.correo)) {
       return window.alert('El formato del email no es correcto!')
     }
+    /* Reemplazar codigo al iniciar sesión para obtener token válido*/
+    /* axios.post(ENDPOINT.login, user)
+      .then(({ data }) => {
+        window.sessionStorage.setItem('token', data.token)
+        window.alert('Usuario identificado con éxito 😀.')
+        setUsuario({})
+        navigate('/perfil')
+      })
+      .catch(({ response: { data } }) => {
+        console.error(data)
+        window.alert(`${data.message} 🙁.`)
+      }) */
+
+    setUsuario(userTemp)
+    navigate('/perfil')
   }
 
   return (
@@ -27,14 +48,14 @@ const Login = () => {
       <div className='container col-10 col-lg-5 text-center bg-white py-3'>
         <h2 className='pb-2'>Inicio de sesión</h2>
         <form onSubmit={handleForm}>
-          <div class='mb-3 px-4'>
-            <label class='form-label'>Correo electrónico</label>
-            <input type='email' name='correo' className='form-control text-center' id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='Correo electrónico de registro' value={user.correo} onChange={handleUser} />
+          <div className='mb-3 px-4'>
+            <label className='form-label'>Correo electrónico</label>
+            <input type='email' name='correo' className='form-control text-center' id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='Correo electrónico de registro' value={userTemp.correo} onChange={handleUser} />
             <div id='emailHelp' className='form-text fst-italic'>Nunca compartiremos tu correo.</div>
           </div>
-          <div class='mb-3 px-4'>
+          <div className='mb-3 px-4'>
             <label className='form-label'>Contraseña</label>
-            <input type='password' name='contrasena' className='form-control text-center' id='exampleInputPassword1' placeholder='*********' value={user.contrasena} onChange={handleUser} />
+            <input type='password' name='contrasena' className='form-control text-center' id='exampleInputPassword1' placeholder='*********' value={userTemp.contrasena} onChange={handleUser} />
           </div>
           <button type='submit' className='btn btn-primary'>Iniciar Sesión</button>
         </form>
