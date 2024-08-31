c06a8abc7ea315846d6f885ad9a653a0ff1372bd
Paula Vergara <paulavergara@hotmail.es>
2024-08-30 20:05:06 -0400

    Se añade a ENDPOINT ruta /tienda y /

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -3,5 +3,7 @@ export const URLBASE = 'http://localhost:3000'
 export const ENDPOINT = {
   login: `${URLBASE}/login`,
   users: `${URLBASE}/usuarios`,
-  registrarse: `${URLBASE}/registrarse`
+  registrarse: `${URLBASE}/registrarse`,
+  tienda: `${URLBASE}/tienda`,
+  home: `${URLBASE}/`
 }
