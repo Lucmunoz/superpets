ee6a2a86fadddb144bbca15ead85da8b24c59511
Paula Vergara <paulavergara@hotmail.es>
2024-08-31 17:09:55 -0400

    Cambios en css

--- frontend/src/views/InformacionPersonal.jsx
+++ frontend/src/views/InformacionPersonal.jsx
@@ -1,9 +1,33 @@
+import { ENDPOINT } from '../config/constants.js'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useContext, useEffect } from 'react'
+import { PetsContext } from '../context/PetsContext.jsx'
+
 const InformacionPersonal = () => {
-  const eliminarcuenta = () => {}
+  const { usuario, cambiarUsuario } = useContext(PetsContext)
+  const navigate = useNavigate()
+  // aqui enviar petición para traer información del usuario
+  const getDeveloperData = () => {
+    const token = window.sessionStorage.getItem('token')
+    axios.get(ENDPOINT.users, { headers: { Authorization: `Bearer ${token}` } })
+      .then(({ data: [user] }) => cambiarUsuario({ ...user }))
+      .catch(({ response: { data } }) => {
+        console.error(data)
+        window.sessionStorage.removeItem('token')
+        cambiarUsuario(null)
+        navigate('/')
+      })
+  }
+
+  useEffect(getDeveloperData, [])
+
+  const eliminarcuenta = () => { window.alert('cuenta eliminada') }
+
   return (
     <main>
       <div className='divInfoPersonal'>
-        <h1>Información Personal</h1>
+        <h1>Información Personal </h1>
         <p>Nombre </p>
         <span>Juan López</span>
         <hr />
@@ -21,7 +45,7 @@ const InformacionPersonal = () => {
         <hr />
         <br />
         <br />
-        <button type='button' className='btn btn-danger' onClick={eliminarcuenta()}>Eliminar Cuenta</button>
+        <button type='button' className='btn btn-danger' onClick={eliminarcuenta}>Eliminar Cuenta</button>
       </div>
     </main>
   )
