3c00d443e8bcf2aaa4679c8e9dc74923c179a816
Paula Vergara <paulavergara@hotmail.es>
2024-09-01 17:44:32 -0400

    Estilos ActualizarPublicacion

--- frontend/src/views/ActualizarPublicacion.jsx
+++ frontend/src/views/ActualizarPublicacion.jsx
@@ -74,35 +74,35 @@ const ActualizarPublicacion = () => {
 
   const mostrarData = () => {
     return (
-      <div className='container-fluid col-11 col-sm-auto my-3 bg-white'>
+      <div className='container-fluid col-11 col-sm-auto my-3 bg-white bordesRed'>
         <div className='p-4 py-4'>
           <div className='d-flex justify-content-center pb-3'>
-            <h2 className=''>Actualizar publicación</h2>
+            <h1 className='tituloForm'>Actualizar publicación <i className='fa-regular fa-pen-to-square ps-2' /></h1>
           </div>
           <form onSubmit={handleForm}>
             <div className='form-group pb-3'>
-              <label htmlFor='exampleFormControlInput1'>Nombre producto </label>
+              <label htmlFor='exampleFormControlInput1' className='labelEstilos'>Nombre producto </label>
               <input type='text' name='nombre' className='d-sm-none form-control fst-italic' placeholder='...' value={publicacionTemporal.nombre} onChange={handlePublicacion} />
               <input type='text' name='nombre' className='d-none d-sm-flex form-control fst-italic' style={{ width: '467px' }} id='nombre' placeholder='...' value={publicacionTemporal.nombre} onChange={handlePublicacion} />
             </div>
             <div className='form-group pb-3'>
-              <label htmlFor='exampleFormControlTextarea1'>Descripción del producto</label>
+              <label htmlFor='exampleFormControlTextarea1' className='labelEstilos'>Descripción del producto</label>
               <textarea className='form-control fst-italic' name='descripcion' rows='3' placeholder='...' value={publicacionTemporal.descripcion} onChange={handlePublicacion} />
             </div>
             <div className='form-group pb-3'>
-              <label htmlFor='exampleFormControlTextarea1'>Precio del producto</label>
+              <label htmlFor='exampleFormControlTextarea1' className='labelEstilos'>Precio del producto</label>
               <input type='number' className='form-control fst-italic' name='precio' placeholder='...' value={publicacionTemporal.precio} onChange={handlePublicacion} />
             </div>
             <div className='form-group pb-3'>
-              <label htmlFor='exampleFormControlInput1'>Ingresa la URL de tu imagen</label>
+              <label htmlFor='exampleFormControlInput1' className='labelEstilos'>Ingresa la URL de tu imagen</label>
               <input type='text' className='form-control fst-italic' name='imagen' placeholder='...' value={publicacionTemporal.imagen} onChange={handlePublicacion} />
               <div className='d-flex justify-content-center p-3'>
                 <img style={{ height: '70px' }} src={publicacionTemporal.imagen} alt='MDN' />
               </div>
             </div>
             <div className='d-flex gap-2 justify-content-center'>
-              <button type='submit' className='btn btn-sm btn-success'>Guardar Cambios</button>
-              <button type='button' className='btn btn-sm btn-danger' onClick={() => goBack()}>Regresar</button>
+              <button type='submit' className='botonEstilos'>Guardar Cambios</button>
+              <button type='button' className='botonEstilos' onClick={() => goBack()}>Regresar</button>
             </div>
           </form>
         </div>
