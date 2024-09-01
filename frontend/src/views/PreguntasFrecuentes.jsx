4af9d0a98d0fe45dc040586b31b28458eb62ad4e
Paula Vergara <paulavergara@hotmail.es>
2024-09-01 13:37:56 -0400

    estilos Preguntas frecuentes

--- frontend/src/views/PreguntasFrecuentes.jsx
+++ frontend/src/views/PreguntasFrecuentes.jsx
@@ -1,7 +1,7 @@
 const PreguntasFrecuentes = () => {
   return (
     <main className='d-flex align-items-centerp'>
-      <div className='container-fluid col-11 col-xl-10 col-xxl-9 my-3 bg-white py-2'>
+      <div className='container-fluid col-11 col-xl-10 col-xxl-9 my-3 bg-white py-2 bordesRed'>
         <div className='container-fluid d-flex flex-column align-items-center col-md-9'>
           <div>
             <h2 className='text-center pt-4'>Preguntas frecuentes <i class='fa-solid fa-question' /></h2>
@@ -15,7 +15,7 @@ const PreguntasFrecuentes = () => {
               Es por ello que habilitamos esta sección de preguntas frecuentes que de seguro te ayudarán a aclarar muchas dudas!
             </p>
             <p>
-              De igual manera, siempre tienes la posibilidad de escribirnos al <a href='#'>mail</a> o hablarnos por <a href='#'>whatsapp</a>!
+              De igual manera, siempre tienes la posibilidad de escribirnos al <a href='mailto:correo@ejemplo.com' style={{ color: '#ED5C01', fontWeight: '500', textDecoration: 'none' }}>mail</a> o hablarnos por<a style={{ color: '#ED5C01', fontWeight: '500', textDecoration: 'none' }} href='https://www.whatsapp.com/' target='_blank' rel='noreferrer'> whatsapp!</a>
             </p>
             <p><span className='fw-bold'>Puedes hacerlo con total confianza!</span></p>
           </div>
@@ -29,7 +29,7 @@ const PreguntasFrecuentes = () => {
                   </button>
                 </h2>
                 <div id='flush-collapseOne' className='accordion-collapse collapse' aria-labelledby='flush-headingOne' data-bs-parent='#accordionFlushExample'>
-                  <div className='accordion-body'>Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                  <div className='accordion-body'>Placeholder content for this accordion, which is intended to demonstrate the <code /> class. This is the first item's accordion body.</div>
                 </div>
               </div>
 
@@ -40,7 +40,7 @@ const PreguntasFrecuentes = () => {
                   </button>
                 </h2>
                 <div id='flush-collapseTwo' className='accordion-collapse collapse' aria-labelledby='flush-headingTwo' data-bs-parent='#accordionFlushExample'>
-                  <div className='accordion-body'>Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                  <div className='accordion-body'>Placeholder content for this accordion, which is intended to demonstrate the <code /> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
                 </div>
               </div>
 
@@ -51,7 +51,7 @@ const PreguntasFrecuentes = () => {
                   </button>
                 </h2>
                 <div id='flush-collapseThree' className='accordion-collapse collapse' aria-labelledby='flush-headingThree' data-bs-parent='#accordionFlushExample'>
-                  <div className='accordion-body'>Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                  <div className='accordion-body'>Placeholder content for this accordion, which is intended to demonstrate the <code /> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
                 </div>
               </div>
 
@@ -62,7 +62,7 @@ const PreguntasFrecuentes = () => {
                   </button>
                 </h2>
                 <div id='flush-collapseFour' className='accordion-collapse collapse' aria-labelledby='flush-headingFour' data-bs-parent='#accordionFlushExample'>
-                  <div className='accordion-body'>Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                  <div className='accordion-body'>Placeholder content for this accordion, which is intended to demonstrate the <code /> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
                 </div>
               </div>
             </div>
