dc74aeed52c69a5e1ad9d4d3dfafd10e6e82b1ff
Paula Vergara <paulavergara@hotmail.es>
2024-09-03 01:09:40 -0400

    Correción en argumento que recibe jwtSign

--- backendPets/src/utils/auth/jwt.js
+++ backendPets/src/utils/auth/jwt.js
@@ -3,6 +3,6 @@ import jwt from 'jsonwebtoken'
 
 const KEY_JWT = process.env.KEY_JWT
 
-export const jwtSign = (email) => jwt.sign(email, KEY_JWT, { expiresIn: 60 * 60 })
+export const jwtSign = (correo) => jwt.sign(correo, KEY_JWT, { expiresIn: 60 * 60 })
 export const jwtVerify = (token) => jwt.verify(token, KEY_JWT)
 export const jwtDecode = (token) => jwt.decode(token)
