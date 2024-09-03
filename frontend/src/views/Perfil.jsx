2192174433b84266ccd227ab46dfb2b7d0c4acc5
Paula Vergara <paulavergara@hotmail.es>
2024-09-03 01:29:29 -0400

    se modifican estilos de Perfil

--- frontend/src/views/Perfil.jsx
+++ frontend/src/views/Perfil.jsx
@@ -41,7 +41,8 @@ const Perfil = () => {
         <div className='d-flex flex-column flex-md-row container-fluid align-items-center justify-content-center p-0'>
           <div className=' text-center'> <img style={{ height: '220px' }} src='/bienvenida.jpg' alt='Imagen perro dando la bienvenida' /></div>
           <div className='list-group text-center pb-3 pt-md-3'>
-            <h1 className='pb-3 fw-bold tituloForm'>Bienvenido {nombre}!</h1>
+            <h1 className='pb-0 fw-bold tituloForm'>¡Bienvenido!</h1>
+            <h2 style={{ color: '#ED5C01', paddingBottom: '20px' }}> {nombre}</h2>
 
             <Link to='/informacionpersonal' className='text-decoration-none'>
               <h2 className='h2PerfilYPersonal'>Información Personal</h2>
