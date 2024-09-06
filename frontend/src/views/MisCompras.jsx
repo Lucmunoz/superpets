ccedfeecd548b30e8cd889b757704ecfe9173586
Paula Vergara <paulavergara@hotmail.es>
2024-09-06 14:16:48 -0400

    Se modifica vista MisCompras(cuando el usuario no ha realizado ninguna compra)

--- frontend/src/views/MisCompras.jsx
+++ frontend/src/views/MisCompras.jsx
@@ -45,10 +45,11 @@ const MisCompras = () => {
       <div className='p-4 bg-white d-flex flex-row justify-content-center align-items-center'>
         <div><img className='img-fluid' src='/carrovacio.jpg' alt='Imagen carro vacío' /></div>
         <div className='d-flex flex-column p-4 text-center'>
-          <h1>Hey!</h1>
-          <h3>Aún no has visto nuestras ofertas?</h3>
-          <h5 className=''>No pierdas mas tiempo, hay increibles descuento esperando! Ellos, lo merecen!</h5>
-          <button type='button' className='btn btn-secondary col-4 me-auto ms-auto'>Ver Productos</button>
+          <h1>¡Hey!</h1>
+          <h2 style={{ fontSize: '22px' }}>¿Aún no has visto nuestras ofertas?</h2>
+          <p>No pierdas mas tiempo, hay increibles productos esperando. <strong>  ¡Ellos, lo merecen!</strong></p>
+          <br />
+          <Link to='/tienda' className='link'> Ir a la Tienda</Link>
         </div>
       </div>
     )
