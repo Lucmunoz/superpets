bca58a1ab589c8f45b7b7bf346e489c9dd37bba8
Lucas <lucas.munoz.vasquez@gmail.com>
2024-09-10 21:42:41 -0300

    se agrega ssl en config de pool

--- backendPets/src/server/database/db_connect.js
+++ backendPets/src/server/database/db_connect.js
@@ -9,8 +9,10 @@ const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  allowExitOnIdle: true
+  allowExitOnIdle: true,
+  ssl: true
 }
+
 
 const pool = new Pool(config)
 
